7c99bd62613f88c4192aa453f4686bef8df1f64f
Graham Moffett <graham.moffett@gmail.com>
2020-11-17 23:40:36 +0000

    Fixed Error with login form, Left Second = of the equal operator in the check login query

--- includes/form_handlers/login_handler.php
+++ includes/form_handlers/login_handler.php
@@ -12,7 +12,7 @@ if(isset($_POST['login_button'])) {
     $check_database_query = mysqli_query($conn, "SELECT * FROM users WHERE email='$email' AND password='$password'");
     $check_login_query = mysqli_num_rows($check_database_query);
 
-    if($check_login_query = 1) {
+    if($check_login_query == 1) {
         $row = mysqli_fetch_array($check_database_query);
         $username = $row['username'];
 
